27f79da364e237b157c56649e92cef370de2680e
sihams <aitoumgharsiham@gmail.com>
2024-12-22 16:25:11 +0100

    add class person

---
+++ src/main/java/com/siham/Person.java
@@ -0,0 +1,41 @@
+package com.siham;
+
+import java.util.Objects;
+
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Person person = (Person) obj;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{id=" + id + ", name='" + name + "'}";
+    }
+}
